032f7b684a16d0540e45ce2053dbe6f7f8b7cf2d
Alessandro Ampala <alessandro.ampala@edu.unito.it>
2020-10-22 16:56:55 +0200

    Added Game class
    Minor changes:
    - added print board debug method

--- Board.py
+++ Board.py
@@ -5,12 +5,24 @@ class Board:
         self._board = []
         # Create empty matrix
         for i in range(self._size):
-            self._board.append([])
+            self._board.append([None] * self._size)
 
     # Get size of the board
     def get_size(self):
         return self._size
 
     # Set the color of a cell
-    def set_cell(self, x, y, player):
-        self._board[x][y] = player.get_color()
+    def set_cell(self, x, y, color):
+        self._board[y][x] = color
+
+    def get_cell(self, x, y):
+        return self._board[y][x]
+
+    def debug_print_board(self):
+        for i in range(self.get_size()):
+            for j in range(self.get_size()):
+                if self.get_cell(i, j) is not None:
+                    print(self.get_cell(i, j), end="")
+                else:
+                    print("-", end="")
+            print()
--- Game.py
+++ Game.py
@@ -1,9 +1,27 @@
 from Board import *
+from Player import *
 
 
-# players:  a list of Player object
-# size: length of a single board row
-# points: points list. Index represents the length, value the points associated
-# win_points: points needed to win a game
-def initialize(players, size, points, win_points):
-    board = Board(size)
+class Game:
+
+    # players:  a list of Player object
+    # size: length of a single board row
+    # points: points list. Index represents the length, value the points associated
+    # win_points: points needed to win a game
+    def __init__(self, players, size, points, win_points):
+        self._board = Board(size)
+        self._players = players
+
+    def get_cell(self, x, y):
+        return self._board.get_cell(x, y)
+
+    def set_cell(self, x, y, player):
+        self._board.set_cell(x, y, player)
+
+    def debug_print_board(self):
+        self._board.debug_print_board()
+
+    # Return true if game ended
+    def game_ended(self):
+        assert False
+        return  # replace with end condition
